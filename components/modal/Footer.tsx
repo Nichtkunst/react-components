02cda21b461b481366a46f6bc8a9980343ea5535
mmso <mmso@mmso.se>
2021-02-25 15:13:01 +0100

    Footer ts

--- components/modal/Footer.tsx
+++ components/modal/Footer.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { classnames } from '../../helpers';
+
+interface Props extends React.ComponentPropsWithoutRef<'footer'> {
+    children?: React.ReactNode;
+    hasConfirmFirst?: boolean;
+}
 
 const Footer = ({
     children,
@@ -10,17 +14,12 @@ const Footer = ({
         hasConfirmFirst ? 'flex-column' : 'flex-justify-space-between flex-align-items-center',
     ]),
     ...rest
-}) => {
+}: Props) => {
     return (
         <footer className={classnames(['modal-footer', className])} {...rest}>
             {children}
         </footer>
     );
-};
-
-Footer.propTypes = {
-    children: PropTypes.node.isRequired,
-    className: PropTypes.string,
 };
 
 export default Footer;
