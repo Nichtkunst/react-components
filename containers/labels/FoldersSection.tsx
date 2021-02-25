5a8514bc5e4a8063370592897b6c3d462f49dabe
Jeremy Benaim <jeremybenaim@gmail.com>
2021-02-25 16:25:43 +0100

    Loaders tweaks + allow forbidden actions for upsell via growler... ¯\_(ツ)_/¯

--- containers/labels/FoldersSection.tsx
+++ containers/labels/FoldersSection.tsx
@@ -13,31 +13,29 @@ function LabelsSection() {
     const [folders = [], loadingFolders] = useFolders();
     const { createModal } = useModals();
 
-    if (loadingFolders) {
-        return (
-            <SettingsSection>
-                <Loader />
-            </SettingsSection>
-        );
-    }
-
     return (
         <SettingsSection>
-            <SettingsParagraph
-                className="mt1 mb1"
-                learnMoreUrl="https://protonmail.com/support/knowledge-base/creating-folders/"
-            >
-                {c('LabelSettings').t`A message can only be filed in a single Folder at a time.`}
-            </SettingsParagraph>
-            <div className="mb1">
-                <Button color="norm" onClick={() => createModal(<EditLabelModal type="folder" />)}>
-                    {c('Action').t`Add folder`}
-                </Button>
-            </div>
-            {folders.length ? (
-                <FolderTreeViewList items={folders} />
+            {loadingFolders ? (
+                <Loader />
             ) : (
-                <SettingsParagraph>{c('LabelSettings').t`No folders available`}</SettingsParagraph>
+                <>
+                    <SettingsParagraph
+                        className="mt1 mb1"
+                        learnMoreUrl="https://protonmail.com/support/knowledge-base/creating-folders/"
+                    >
+                        {c('LabelSettings').t`A message can only be filed in a single Folder at a time.`}
+                    </SettingsParagraph>
+                    <div className="mb1">
+                        <Button color="norm" onClick={() => createModal(<EditLabelModal type="folder" />)}>
+                            {c('Action').t`Add folder`}
+                        </Button>
+                    </div>
+                    {folders.length ? (
+                        <FolderTreeViewList items={folders} />
+                    ) : (
+                        <SettingsParagraph>{c('LabelSettings').t`No folders available`}</SettingsParagraph>
+                    )}
+                </>
             )}
         </SettingsSection>
     );
