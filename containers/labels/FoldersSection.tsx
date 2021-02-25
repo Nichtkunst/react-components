6c0f0d081e83ddeafa5608b072d6776eba617a1f
Jeremy Benaim <jeremybenaim@gmail.com>
2021-02-25 08:35:08 +0100

    Folders / Labels page

--- containers/labels/FoldersSection.tsx
+++ containers/labels/FoldersSection.tsx
@@ -1,63 +1,45 @@
 import React from 'react';
 import { c } from 'ttag';
 
-import { Loader, Alert, PrimaryButton, Label, Row, Field, Info } from '../../components';
-import { useFolders, useMailSettings, useModals } from '../../hooks';
+import { Loader, Button } from '../../components';
+import { useFolders, useModals } from '../../hooks';
+
+import { SettingsSection, SettingsParagraph } from '../account';
+
 import FolderTreeViewList from './FolderTreeViewList';
 import EditLabelModal from './modals/EditLabelModal';
-import ToggleEnableFolderColor from './ToggleEnableFolderColor';
-import ToggleInheritParentFolderColor from './ToggleInheritParentFolderColor';
 
 function LabelsSection() {
-    const [folders, loadingFolders] = useFolders();
+    const [folders = [], loadingFolders] = useFolders();
     const { createModal } = useModals();
-    const [mailSettings] = useMailSettings();
 
     if (loadingFolders) {
-        return <Loader />;
+        return (
+            <SettingsSection>
+                <Loader />
+            </SettingsSection>
+        );
     }
 
     return (
-        <>
-            <Alert
-                type="info"
+        <SettingsSection>
+            <SettingsParagraph
                 className="mt1 mb1"
-                learnMore="https://protonmail.com/support/knowledge-base/creating-folders/"
+                learnMoreUrl="https://protonmail.com/support/knowledge-base/creating-folders/"
             >
                 {c('LabelSettings').t`A message can only be filed in a single Folder at a time.`}
-            </Alert>
-            <Row>
-                <Label htmlFor="folder-colors">{c('Label').t`Use folder colors`}</Label>
-                <Field>
-                    <ToggleEnableFolderColor id="folder-colors" />
-                </Field>
-            </Row>
-            {mailSettings?.EnableFolderColor ? (
-                <Row>
-                    <Label htmlFor="parent-folder-color">
-                        <span>{c('Label').t`Inherit parent folder color`}</span>
-                        <Info
-                            buttonClass="ml0-5 inline-flex"
-                            title={c('Info')
-                                .t`This feature automates folder coloring. When turned on, sub-folders inherit the color of the first level folder.`}
-                        />
-                    </Label>
-                    <Field>
-                        <ToggleInheritParentFolderColor id="parent-folder-color" />
-                    </Field>
-                </Row>
-            ) : null}
+            </SettingsParagraph>
             <div className="mb1">
-                <PrimaryButton onClick={() => createModal(<EditLabelModal type="folder" />)}>
+                <Button color="norm" onClick={() => createModal(<EditLabelModal type="folder" />)}>
                     {c('Action').t`Add folder`}
-                </PrimaryButton>
+                </Button>
             </div>
-            {folders?.length ? (
+            {folders.length ? (
                 <FolderTreeViewList items={folders} />
             ) : (
-                <Alert>{c('LabelSettings').t`No folders available`}</Alert>
+                <SettingsParagraph>{c('LabelSettings').t`No folders available`}</SettingsParagraph>
             )}
-        </>
+        </SettingsSection>
     );
 }
 
